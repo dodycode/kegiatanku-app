03b5067af1c462ccb88d51f6ded4b4fa79a04d70
Dodycode <prasetyodody17@gmail.com>
2019-12-01 01:34:45 +0800

    implements OneSignal for push notif

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import { MenuProvider } from 'react-native-popup-menu';
 import Routes from './src/screens/routes';
+import OneSignal from 'react-native-onesignal';
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-
+		OneSignal.init("8de9d54b-3ba6-4ae3-9fc4-d4236c76a13c");
 		this.state = {};
 	}
 
